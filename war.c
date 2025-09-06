341a8aa1973ed066befce6c989556227d7a6888b
Kauancode12 <kakabranco39@gmail.com>
2025-09-06 18:38:49 +0000

    fim "versao recuperada nivel aventureiro"

--- war.c
+++ war.c
@@ -10,89 +10,6 @@ typedef struct  {
     int tropas;
 } Territorio;
 
-void cadastrar (Territorio *planeta, int n);
-void limparBufferEntrada();
-void exibirMenu();
-void atacar();
-void jogar(Territorio *planeta, int n, int opcao);
-
-// FUNÇÃO PRINCIPAL
-int main() {
-    srand(time(NULL));
-
-    int opcao;
-    int n;
-
-    // PERGUNTA O NÚMERO DE TERRITORIOS E GUARDA NA VARIAVEL n QUE SERÁ USADA 
-    // DENTRO DO calloc PARA ALOCAR MEMORIA PARA ACOMODAR OS "N" TERRITORIOS
-    printf("Quantos territorios deseja cadastrar?: ");
-    scanf("%d", &n);
-    limparBufferEntrada();
-
-    Territorio *planeta;//declara ponteiro
-    planeta = (Territorio*) calloc(n, sizeof(Territorio));//aloca memoria para guardar os territorios conforme o n
-    
-    // VALIDA A ALOCAÇÃO DE MEMORIA
-    if (planeta == NULL) {
-        printf("Erro ao alocar memoria.\n");
-        return 1;
-    }
- 
-    
-    
-    // função free usada para liberar memoria anteriormente alocada por calloc
-    // isso evita vazamento de memoria
-    free(planeta);
-    printf("Programa encerrado. \n");
-
-    return 0;
-}
-
-// CÓDIGO PARA LIMPAR O BUFFER EVITANDO ERROS COM scanf
-void limparBufferEntrada() {
-    int c;
-    while ((c = getchar()) != '\n' && c != EOF);
-}
-
-void exibirMenu() {
-    printf("====== Jogo War ======\n");
-    printf("1 - Jogar\n");
-    printf("2 - Verificar missão\n");
-    printf("0 - Sair");
-}
-
-// LOOP for PARA SAIDA DE DADOS NO CONSOLE COM printf, PERGUNTA E GUARDA AS INFORMAÇÕES DOS TERRITORIOS
-void cadastrar (Territorio *planeta, int n) {
-    // ENTRADA DE DADOS COM printf
-    printf("\n======================================= \n");
-    printf("CADASTRANDO TERRITORIOS PARA O SEU JOGO \n");
-    printf("======================================= \n");
-
-    for (int i = 0; i < n; i++) {
-        printf("\nCADASTRO DO TERRITORIO %d\n", i + 1);
-        
-        // Pergunta nome, cor do exercito e quantidade de tropas para o usuario cadastrar os territorios
-        printf("Nome: ");
-        fgets(planeta[i].nome, 30, stdin);
-
-        printf("Cor do exercito: ");
-        fgets(planeta[i].cor, 10, stdin);
-
-        planeta[i].nome[strcspn(planeta[i].nome, "\n")] = '\0';
-        planeta[i].cor[strcspn(planeta[i].cor, "\n")] = '\0';
-
-        printf("Quantidade de tropas: ");
-        scanf("%d", &planeta[i].tropas);
-        limparBufferEntrada();// LIMPA O ENTER QUE FICA NO BUFFER
-
-    }
-
-        printf("\n=================================== \n");
-        printf("CADASTRO DOS TERRITORIOS CONCLUIDOS \n");
-        printf("=================================== \n");
-
-}
-
 // FUNÇÃO QUE SIMULA UM ATAQUE COM TODAS AS REGRAS
 void atacar(Territorio *atacante, Territorio *defensor) {
     int dadoAtq = (rand() %6) + 1;
@@ -117,10 +34,65 @@ void atacar(Territorio *atacante, Territorio *defensor) {
     }
 
 }
+// CÓDIGO PARA LIMPAR O BUFFER EVITANDO ERROS COM scanf
+void limparBufferEntrada() {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+}
 
-// LOOP for QUE PERGUNTA AO USUARIO O ATACANTE E O DEFENSOR, COM FUNÇÕES DE VALIDAÇÃO DE ESCOLHAS
-void jogar(Territorio *planeta, int n, int opcao) {
-     do {
+// FUNÇÃO PRINCIPAL
+int main() {
+    srand(time(NULL));
+
+    int opcao;
+    int n;
+
+    // PERGUNTA O NÚMERO DE TERRITORIOS E GUARDA NA VARIAVEL n QUE SERÁ USADA 
+    // DENTRO DO calloc PARA ALOCAR MEMORIA PARA ACOMODAR OS "N" TERRITORIOS
+    printf("Quantos territorios deseja cadastrar?: ");
+    scanf("%d", &n);
+    limparBufferEntrada();
+
+    Territorio *planeta;//declara ponteiro
+    planeta = (Territorio*) calloc(n, sizeof(Territorio));//aloca memoria para guardar os territorios conforme o n
+    
+    // VALIDA A ALOCAÇÃO DE MEMORIA
+    if (planeta == NULL) {
+        printf("Erro ao alocar memoria.\n");
+        return 1;
+    }
+ 
+    // ENTRADA DE DADOS COM printf
+    printf("\n======================================= \n");
+    printf("CADASTRANDO TERRITORIOS PARA O SEU JOGO \n");
+    printf("======================================= \n");
+    
+    // LOOP for PARA SAIDA DE DADOS NO CONSOLE COM printf, PERGUNTA E GUARDA AS INFORMAÇÕES DOS TERRITORIOS
+    for (int i = 0; i < n; i++) {
+        printf("\nCADASTRO DO TERRITORIO %d\n", i + 1);
+        
+        // Pergunta nome, cor do exercito e quantidade de tropas para o usuario cadastrar os territorios
+        printf("Nome: ");
+        fgets(planeta[i].nome, 30, stdin);
+
+        printf("Cor do exercito: ");
+        fgets(planeta[i].cor, 10, stdin);
+
+        planeta[i].nome[strcspn(planeta[i].nome, "\n")] = '\0';
+        planeta[i].cor[strcspn(planeta[i].cor, "\n")] = '\0';
+
+        printf("Quantidade de tropas: ");
+        scanf("%d", &planeta[i].tropas);
+        limparBufferEntrada();// LIMPA O ENTER QUE FICA NO BUFFER
+
+    }
+
+        printf("\n=================================== \n");
+        printf("CADASTRO DOS TERRITORIOS CONCLUIDOS \n");
+        printf("=================================== \n");
+
+    // LOOP for QUE PERGUNTA AO USUARIO O ATACANTE E O DEFENSOR, COM FUNÇÕES DE VALIDAÇÃO DE ESCOLHAS
+    do {
         printf("\n==Territorios==\n");
         for (int i = 0; i < n; i++) {
             printf("%d - %s (%d tropas do exercito %s)\n", i + 1, planeta[i].nome, planeta[i].tropas, planeta[i].cor);
@@ -139,17 +111,26 @@ void jogar(Territorio *planeta, int n, int opcao) {
         //funções de segurança
         if (atq < 0 || atq >= n || def < 0 || def >= n || atq == def) {
             printf("Escolha invalida\n");
+            printf("Pressione Enter para prosseguir...");
+            limparBufferEntrada();
+            getchar();
             continue;
         }
 
         if (planeta[atq].tropas <= 1) {
             printf("Voce não pode atacar com apenas 1 tropa\n");
+            printf("Pressione Enter para prosseguir...");
+            limparBufferEntrada();
+            getchar();
             continue;
         }
 
         //função que bloqueia o atacante de atacar um territorio de sua posse
         if (strcmp(planeta[atq].cor, planeta[def].cor) == 0) {
             printf("Voce não pode atacar um territorio que já conquistou\n");
+            printf("Pressione Enter para prosseguir...");
+            limparBufferEntrada();
+            getchar();
             continue;
         }
 
@@ -162,4 +143,11 @@ void jogar(Territorio *planeta, int n, int opcao) {
         getchar();
 
     } while (1);
+
+    // função free usada para liberar memoria anteriormente alocada por calloc
+    // isso evita vazamento de memoria
+    free(planeta);
+    printf("Programa encerrado. \n");
+
+    return 0;
 }
